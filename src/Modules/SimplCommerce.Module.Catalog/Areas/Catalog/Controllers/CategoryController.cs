7d98b82b5fd4e38077383aba7b03bc6bd1ba1849
Toan Nguyen <Password@01>
2019-04-04 00:31:42 +0700

    Remove path

--- src/Modules/SimplCommerce.Module.Catalog/Areas/Catalog/Controllers/CategoryController.cs
+++ src/Modules/SimplCommerce.Module.Catalog/Areas/Catalog/Controllers/CategoryController.cs
@@ -36,7 +36,6 @@ namespace SimplCommerce.Module.Catalog.Areas.Catalog.Controllers
             _productPricingService = productPricingService;
             _pageSize = config.GetValue<int>("Catalog.ProductPageSize");
         }
-
         public IActionResult CategoryDetail(long id, SearchOption searchOption)
         {
             var category = _categoryRepository.Query().FirstOrDefault(x => x.Id == id);
@@ -138,6 +137,113 @@ namespace SimplCommerce.Module.Catalog.Areas.Catalog.Controllers
             model.CurrentSearchOption.Page = currentPageNum;
 
             return View(model);
+        }
+        [HttpGet("/")]
+        public IActionResult CategoryHome(long id, SearchOption searchOption)
+        {
+            if (id == 0)
+            {
+                id = 7;
+            }
+            var category = _categoryRepository.Query().FirstOrDefault(x => x.Id == id);
+            if (category == null)
+            {
+                return Redirect("~/Error/FindNotFound");
+            }
+
+            var model = new ProductsByCategory
+            {
+                CategoryId = category.Id,
+                ParentCategorId = category.ParentId,
+                CategoryName = category.Name,
+                CategorySlug = category.Slug,
+                CategoryMetaTitle = category.MetaTitle,
+                CategoryMetaKeywords = category.MetaKeywords,
+                CategoryMetaDescription = category.MetaDescription,
+                CurrentSearchOption = searchOption,
+                FilterOption = new FilterOption()
+            };
+
+            var query = _productRepository
+                .Query()
+                // .OrderByDescending(m=>m.CreatedOn)
+                .Where(x => x.Categories.Any(c => c.CategoryId == category.Id) && x.IsPublished && x.IsVisibleIndividually);
+
+            if (query.Count() == 0)
+            {
+                model.TotalProduct = 0;
+                return View(model);
+            }
+
+            AppendFilterOptionsToModel(model, query);
+
+            if (searchOption.MinPrice.HasValue)
+            {
+                query = query.Where(x => x.Price >= searchOption.MinPrice.Value);
+            }
+
+            if (searchOption.MaxPrice.HasValue)
+            {
+                query = query.Where(x => x.Price <= searchOption.MaxPrice.Value);
+            }
+
+            if (string.Compare(model.CurrentSearchOption.Category, "all", StringComparison.OrdinalIgnoreCase) != 0)
+            {
+                var categories = searchOption.GetCategories();
+                if (categories.Any())
+                {
+                    var categoryIds = _categoryRepository.Query().Where(x => categories.Contains(x.Slug)).Select(x => x.Id).ToList();
+                    query = query.Where(x => x.Categories.Any(c => categoryIds.Contains(c.CategoryId)));
+                }
+            }
+            var brands = searchOption.GetBrands();
+            if (brands.Any())
+            {
+                var brandIds = _brandRepository.Query().Where(x => brands.Contains(x.Slug)).Select(x => x.Id).ToList();
+                query = query.Where(x => x.BrandId.HasValue && brandIds.Contains(x.BrandId.Value));
+            }
+
+            model.TotalProduct = query.Count();
+            var currentPageNum = searchOption.Page <= 0 ? 1 : searchOption.Page;
+            var offset = (_pageSize * currentPageNum) - _pageSize;
+            while (currentPageNum > 1 && offset >= model.TotalProduct)
+            {
+                currentPageNum--;
+                offset = (_pageSize * currentPageNum) - _pageSize;
+            }
+
+            query = query
+                .Include(x => x.Brand)
+                .Include(x => x.Categories).ThenInclude(x => x.Category)
+                .Include(x => x.ThumbnailImage)
+                .Include(x => x.PromotionImage);
+
+            query = ApplySort(searchOption, query);
+
+            var products = query
+                .Select(x => ProductThumbnail.FromProduct(x))
+                .Skip(offset)
+                .Take(_pageSize)
+                .ToList();
+
+            foreach (var product in products)
+            {
+                product.ThumbnailUrl = _mediaService.GetThumbnailUrl(product.ThumbnailImage);
+
+                if (product.PromotionImage != null)
+                {
+                    product.PromotionImageUrl = _mediaService.GetPromotionUrl(product.PromotionImage);
+                    product.PromotionImage = product.PromotionImage;
+                }
+                product.CalculatedProductPrice = _productPricingService.CalculateProductPrice(product);
+            }
+
+            model.Products = products;
+            model.CurrentSearchOption.PageSize = _pageSize;
+            model.CurrentSearchOption.PageCount = System.Convert.ToInt32(System.Math.Ceiling(model.TotalProduct / System.Convert.ToDouble(_pageSize)));
+            model.CurrentSearchOption.Page = currentPageNum;
+
+            return View("CategoryDetail",model);
         }
 
         public IActionResult CategoryDetailLoadMore(long id, SearchOption searchOption)
--- src/Modules/SimplCommerce.Module.Core/Areas/Core/Controllers/HomeController.cs
+++ src/Modules/SimplCommerce.Module.Core/Areas/Core/Controllers/HomeController.cs
@@ -26,7 +26,7 @@ namespace SimplCommerce.Module.Core.Areas.Core.Controllers
             throw new Exception("Test behavior in case of error");
         }
 
-        [HttpGet("/")]
+        //[HttpGet("/")]
         public IActionResult Index()
         {
             if (DateTime.Today > DateTime.Parse("19/06/2019"))
